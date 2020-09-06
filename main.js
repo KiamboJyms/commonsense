e6993eea5c24c8d94f578d11cde1a0903fe24b09
KiamboJyms <67885723+KiamboJyms@users.noreply.github.com>
2020-09-07 02:42:16 +0300

    upload

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let win;
  function createWindow(){
      //create the browser window
      win = new BrowserWindow({width: 800,
-                              height: 600
+                              height: 600,
+                               webPreferences: {nodeIntegration: true}
                             })
 
      win.loadURL(url.format({
          pathname: path.join(__dirname, './dist/atatest/index.html'),
          protocol: 'file:',
-         slashes: true,
-         webPreferences: {nodeIntegration: true}
+         slashes: true
      }))
 
 
